58341b774050ab2efc1b8c4e0bb58365ae6ad78b
pufomadu <72574559+pufomadu@users.noreply.github.com>
2020-12-11 06:47:23 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -2,26 +2,41 @@ import pytest
 from bmi import *
 from health_app import *
 
-#Testing BMI Function
+
+# Testing BMI Function
 @pytest.mark.parametrize('height, weight, h_system, w_system',
                          [
                              (180, 165, 'metric', 'imperial'),
                              (152.4, 118.9, 'metric', 'imperial'),
                          ])
 def test_bmi(height, weight, h_system, w_system):
-   p = bmi(height = float(height), weight = float(weight), h_system = str(h_system), w_system = str(w_system))
+    p = bmi(height=float(height), weight=float(weight), h_system=str(h_system), w_system=str(w_system))
 
-   assert True
+    assert True
 
 
-#Testing BMR Function
+# Testing BMR Function
 @pytest.mark.parametrize('height, weight, age, sex, activity, h_system, w_system',
                          [
-                             (180, 60, 22,'m', 2, 'metric', 'metric'),
-                             (60.1, 118.9, 'f',22, 3, 'imperial', 'imperial'),
+                             (180, 60, 22, 'm', 2, 'metric', 'metric'),
+                             (60.1, 118.9, 'f', 22, 3, 'imperial', 'imperial'),
                          ])
 def test_bmr(height, weight, age, sex, activity, h_system, w_system):
-   m = bmr(height = float(height), weight = float(weight), age= float(age), sex = str(sex), activity = int(activity), h_system = str(h_system), w_system = str(w_system))
-   
-   assert True
-   
+    m = bmr(height=float(height), weight=float(weight), age=float(age), sex=str(sex), activity=int(activity),
+            h_system=str(h_system), w_system=str(w_system))
+
+    assert True
+
+#test calorie addition function
+@pytest.mark.parametrize("output",
+                         [
+                             ("Ice cream, rich - cup", "Soft serve ice cream - cup")
+                         ])
+def display_total_calories(output):
+   x = display_total_calories(output)
+   assert 716.25 == x
+
+
+if __name__ == '__main__':
+    pass
+
